08c6854e14238a8d3c79c4fa08ff47147dd6b492
IzumiiHD <technoscootalo@gmail.com>
2025-09-01 00:04:24 -0400

    changed port

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ app.use('/storeWebhook', express.raw({ type: 'application/json' }));
 app.use(router);
 
 const hostname = 'localhost';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const encpass = process.env["encpass"];
 
